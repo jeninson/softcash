9c1488421a219159eaa409de0810e3592f9d2b14
Jeninson <jeninson@gmail.com>
2025-03-18 21:56:08 -0500

    Agregando Metodo de POST a la API de Login

--- api/login/login.php
+++ api/login/login.php
@@ -1,9 +1,21 @@
 <?php
     require_once '../configbd/db.php';
-    try {
-        $base = new Db();
-        $conn = $base->conectar();
-    } catch (Exception $e) {
-        echo "ERROR=>".$e->getMessage();
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $data = json_decode(file_get_contents("php://input"));
+        try {
+            $base = new Db();
+            $conn = $base->conectar();
+            header("HTTP/1.1 200 OK");
+            echo json_encode(array("code"=>200, "msg" => "Conexión exitosa"));
+        } catch (Exception $e) {
+            header("HTTP/1.1 500 Internal Server Error");
+            echo json_encode(array("code"=>500, "msg" => "Error en el servidor \n".$e->getMessage()));
+        }
+    } else {
+        header("HTTP/1.1 400 Bad Request");
+        echo json_encode(array("code"=>400, "msg" => "Solicitud incorrecta por parte del cliente"));
     }
+
+    
 ?>
